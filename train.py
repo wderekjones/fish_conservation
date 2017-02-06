31d3e1fd76b8ad2729f0a2ec7abd493c9c1a08c8
williamdjones <derek.jones4@uky.edu>
2017-02-06 11:57:29 -0800

    defined new models and in the process of adding checkpoints

--- train.py
+++ train.py
@@ -1,12 +1,18 @@
 import numpy as np
-from models import model1
+from models import*
 
 from utils import *
+
+import time
+import h5py
 
 
 from sklearn.model_selection import train_test_split
 
 from skimage import feature
+
+from keras.optimizers import Adam
+from keras.callbacks import LearningRateScheduler, ModelCheckpoint
 
 import matplotlib.pyplot as plt
 
@@ -22,10 +28,17 @@ X = compute_canny(X,3)
 #X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33)
 
 
+date = time.strftime("%m/%d/%Y")
+
+time = time.strftime('%H:%M:%S', time.gmtime())
+
+model_name = "checkpoints/model"+date+time
+
+checkpointer = ModelCheckpoint(filepath=model_name, monitor='val_loss')
 
 model = model1()
 
-model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=0.0001),loss='categorical_crossentropy',metrics=['accuracy'])
 
-model.fit(X,y,batch_size=10,nb_epoch=100,shuffle=True)
+model.fit(X,y,batch_size=10,nb_epoch=100,shuffle=True, callbacks=[checkpointer])
 
