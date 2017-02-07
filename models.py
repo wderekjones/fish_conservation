ce5c7989a1d325fe4d72bafa98d5e98760ee35c5
williamdjones <derek.jones4@uky.edu>
2017-02-06 18:44:20 -0800

    fixing errors

--- models.py
+++ models.py
@@ -1,19 +1,32 @@
 from keras.models import Sequential
 
-from keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D,Flatten
+from keras.layers import Dense, Dropout, Convolution2D, MaxPooling2D,Flatten,Activation
 from keras.layers.advanced_activations import PReLU
 
 
+def model0():
+    model = Sequential()
+    model.add(Convolution2D(nb_filter=1,nb_row=5,nb_col=7,input_shape=(600,600,1),activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Flatten())
+    model.add(Dense(output_dim=8,activation='softmax'))
+
+    return model
+
 def model1():
     model = Sequential()
-    model.add(Convolution2D(nb_filter=30,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
+    model.add(Convolution2D(nb_filter=30,nb_row=5,nb_col=7,input_shape=(600,600,1)))
+    model.add(PReLU())
     model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,activation=PReLU()))
-    model.add(Convolution2D(nb_filter=20,nb_row=5,nb_col=7,activation=PReLU()))
+    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7))
+    model.add(PReLU())
+    model.add(Convolution2D(nb_filter=20,nb_row=5,nb_col=7))
+    model.add((PReLU()))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     model.add(Flatten())
-    model.add(Dense(output_dim=10,activation=PReLU()))
+    model.add(Dense(output_dim=10))
+    model.add(PReLU())
     model.add(Dense(output_dim=8,activation='softmax'))
 
     return model
@@ -21,6 +34,7 @@ def model1():
 def model2():
     model = Sequential()
     model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
+
     model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
     model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
     model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
--- train.py
+++ train.py
@@ -28,11 +28,14 @@ X = compute_canny(X,3)
 #X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33)
 
 
-date = time.strftime("%m/%d/%Y")
+date = time.strftime("%m:%d:%Y")
 
 time = time.strftime('%H:%M:%S', time.gmtime())
 
 model_name = "checkpoints/model"+date+time
+
+model_file = open(model_name,mode='w')
+
 
 checkpointer = ModelCheckpoint(filepath=model_name, monitor='val_loss')
 
@@ -40,5 +43,6 @@ model = model1()
 
 model.compile(optimizer=Adam(lr=0.0001),loss='categorical_crossentropy',metrics=['accuracy'])
 
-model.fit(X,y,batch_size=10,nb_epoch=100,shuffle=True, callbacks=[checkpointer])
+model.fit(X,y,batch_size=10,nb_epoch=100,shuffle=True,validation_split=0.2, callbacks=[checkpointer])
 
+model_file.close()
