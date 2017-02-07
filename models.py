47c6b4edc4f0e27ce558b873af3cfef6b08f5d08
williamdjones <derek.jones4@uky.edu>
2017-02-07 09:14:21 -0800

    changes

--- models.py
+++ models.py
@@ -38,15 +38,20 @@ def model1():
 
 def model2():
     model = Sequential()
-    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
-
-    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
-    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
-    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
-    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1),activation=PReLU()))
+    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1)))
+    model.add(PReLU())
+    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1)))
+    model.add(PReLU())
+    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1)))
+    model.add(PReLU())
+    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1)))
+    model.add(PReLU())
+    model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7,input_shape=(600,600,1)))
+    model.add(PReLU())
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Flatten())
-    model.add(Dense(output_dim=10,activation=PReLU()))
+    model.add(Dense(output_dim=10))
+    model.add(PReLU())
     model.add(Dense(output_dim=8,activation='softmax'))
 
     return model
