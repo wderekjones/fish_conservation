81165210301263151c617c2fa6fb61231b7b0a12
williamdjones <derek.jones4@uky.edu>
2017-02-06 23:23:23 -0800

    changes

--- models.py
+++ models.py
@@ -17,16 +17,21 @@ def model1():
     model = Sequential()
     model.add(Convolution2D(nb_filter=30,nb_row=5,nb_col=7,input_shape=(600,600,1)))
     model.add(PReLU())
+    model.add(Dropout(0.5))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(Convolution2D(nb_filter=10,nb_row=5,nb_col=7))
     model.add(PReLU())
+    model.add(Dropout(0.5))
     model.add(Convolution2D(nb_filter=20,nb_row=5,nb_col=7))
     model.add((PReLU()))
+    model.add(Dropout(0.5))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     model.add(Flatten())
+    model.add(Dropout(0.5))
     model.add(Dense(output_dim=10))
     model.add(PReLU())
+    model.add(Dropout(0.5))
     model.add(Dense(output_dim=8,activation='softmax'))
 
     return model
